a806003172a7b01208c94f971a23d06a5415f1ab
LuisPADD <maryofgod1706@gmail.com>
2026-07-31 17:46:46 -0300

    Add test for opening URL and verifying page title

--- tests/test_open_url.py
+++ tests/test_open_url.py
@@ -1,1 +1,5 @@
 from playwright.sync_api import Page, expect
+
+def test_open_url(page: Page):
+    page.goto("https://leogcarvalho.github.io/test-automation-practice/")
+    expect(page).to_have_title("Test Automation Practice Page")
