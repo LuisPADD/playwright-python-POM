09362b54230d62de1dbbabbaeaa1b52c88c97718
LuisPADD <maryofgod1706@gmail.com>
2026-08-04 15:02:59 -0300

    test(playwright): Python Locators: add interaction with radio button in open URL test

--- tests/test_open_url.py
+++ tests/test_open_url.py
@@ -4,5 +4,7 @@ import time
 def test_open_url(page: Page):
     page.goto("https://leogcarvalho.github.io/test-automation-practice/")
     expect(page).to_have_title("Test Automation Practice Page")
+    page.get_by_role("radio",name="Option A").click()
+    time.sleep(3)
 
 
