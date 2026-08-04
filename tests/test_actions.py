96f913c49abc832a0b3f64cbfa9efc2e909e13ac
LuisPADD <maryofgod1706@gmail.com>
2026-08-04 17:14:04 -0300

    test(playwright): checks and to_be_checked()

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -2,10 +2,17 @@ from playwright.sync_api import Page, expect
 
 def test_actions(page: Page) -> None:
     page.goto("https://leogcarvalho.github.io/test-automation-practice/")
-    # page.get_by_role("textbox", name="Username: (admin)").fill("admin")
-    # page.get_by_role("textbox", name="Password: (1234)").fill("1234")
+    #page.get_by_role("textbox", name="Username: (admin)").fill("admin")
+    #page.get_by_role("textbox", name="Password: (1234)").fill("1234")
+
+
+    #Press_sequentially
     page.get_by_role("textbox", name="Username: (admin)").press_sequentially("admin",delay=500)
     page.get_by_role("textbox", name="Password: (1234)").press_sequentially("1234",delay=500)
     page.get_by_role("textbox", name="Select a date:").press_sequentially("10102000",delay=500)
-    page.get_by_role("button", name="Login").click()
-    page.get_by_text("Login successful!").click()
+
+    # check
+    page.get_by_role("checkbox",name="Feature 1").check()
+    expect(page.get_by_role("checkbox",name="Feature 1")).to_be_checked()
+    page.get_by_role("radio",name="Option B").check()
+    expect(page.get_by_role("radio",name="Option B")).to_be_checked()
