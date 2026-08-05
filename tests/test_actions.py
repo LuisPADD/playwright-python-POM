3a217a0979664119ab16df41e856804bcaa2b80b
LuisPADD <maryofgod1706@gmail.com>
2026-08-05 17:48:14 -0300

    test(playwright): mouse action tests

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -7,9 +7,9 @@ def test_actions(page: Page) -> None:
 
 
     #Press_sequentially
-    page.get_by_role("textbox", name="Username: (admin)").press_sequentially("admin",delay=500)
-    page.get_by_role("textbox", name="Password: (1234)").press_sequentially("1234",delay=500)
-    page.get_by_role("textbox", name="Select a date:").press_sequentially("10102000",delay=500)
+    page.get_by_role("textbox", name="Username: (admin)").press_sequentially("admin",delay=100)
+    page.get_by_role("textbox", name="Password: (1234)").press_sequentially("1234",delay=100)
+    page.get_by_role("textbox", name="Select a date:").press_sequentially("10102000",delay=100)
 
     # check
     page.get_by_role("checkbox",name="Feature 1").check()
@@ -19,3 +19,16 @@ def test_actions(page: Page) -> None:
 
     # select_option
     page.get_by_label("Choose an option:").select_option(value="option3")
+
+    # mouse actions
+    page.get_by_text("single Click").click()
+    expect(page.get_by_text("Single Click button clicked successfully!")).to_be_visible()
+    page.get_by_text("Double Click").dblclick()
+    expect(page.get_by_text("Double Click button clicked successfully!")).to_be_visible()
+    page.get_by_text("Right Click").click(button="right")
+    expect(page.get_by_text("Right click button clicked successfully!")).to_be_visible()
+    page.get_by_text("Shift + Click").click(modifiers=["Shift"])
+    expect(page.get_by_text("Shift + click button clicked successfully!")).to_be_visible()
+    page.get_by_text("Hover Menu").hover()
+    page.locator('//*[@class="dropdown-content"]/*[text()="Option 1"]').click()
+    expect(page).to_have_url("https://leogcarvalho.github.io/test-automation-practice/#option1")
