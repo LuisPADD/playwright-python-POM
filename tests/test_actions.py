4c6f8b23103350e1080d34b63a09688d4533ded7
LuisPADD <maryofgod1706@gmail.com>
2026-08-05 17:56:23 -0300

    test(playwright): specific key press commands

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -2,14 +2,22 @@ from playwright.sync_api import Page, expect
 
 def test_actions(page: Page) -> None:
     page.goto("https://leogcarvalho.github.io/test-automation-practice/")
+    
     #page.get_by_role("textbox", name="Username: (admin)").fill("admin")
     #page.get_by_role("textbox", name="Password: (1234)").fill("1234")
+    
+    # fill & keys
+    page.get_by_role("textbox", name="Username: (admin)").fill("admin")
+    page.get_by_role("textbox", name="Username: (admin)").press("Tab")
+    page.get_by_role("textbox", name="Password: (1234)").fill("1234")
+    page.get_by_role("textbox", name="Password: (1234)").press("Enter")
+    expect(page.get_by_text("Login successful!")).to_be_visible()
 
 
     #Press_sequentially
-    page.get_by_role("textbox", name="Username: (admin)").press_sequentially("admin",delay=100)
-    page.get_by_role("textbox", name="Password: (1234)").press_sequentially("1234",delay=100)
-    page.get_by_role("textbox", name="Select a date:").press_sequentially("10102000",delay=100)
+    # page.get_by_role("textbox", name="Username: (admin)").press_sequentially("admin",delay=100)
+    # page.get_by_role("textbox", name="Password: (1234)").press_sequentially("1234",delay=100)
+    # page.get_by_role("textbox", name="Select a date:").press_sequentially("10102000",delay=100)
 
     # check
     page.get_by_role("checkbox",name="Feature 1").check()
@@ -32,3 +40,5 @@ def test_actions(page: Page) -> None:
     page.get_by_text("Hover Menu").hover()
     page.locator('//*[@class="dropdown-content"]/*[text()="Option 1"]').click()
     expect(page).to_have_url("https://leogcarvalho.github.io/test-automation-practice/#option1")
+
+
